2b963659bfabb3f74dd4ae4cf12559352e3885c7
shaiksohaib <sm.sohaib01@gmail.com>
2021-09-19 20:26:30 +0530

    adding countries

---
+++ Countries.js
@@ -0,0 +1,18 @@
+const countries = [
+    'ALBANIA',
+    'BOLIVIA',
+    'CANADA',
+    'DENMARK',
+    'ETHIOPIA',
+    'FINLAND',
+    'GERMANY',
+    'HUNGARY',
+    'IRELAND',
+    'JAPAN',
+    'KENYA'
+]
+
+for (var i = 0; i < countries.length; i++) {
+    console.log([countries[i], countries[i].slice(0, 3), countries[i].length]);
+}
+
